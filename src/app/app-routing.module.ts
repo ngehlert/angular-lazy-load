a5947ddab5de2983543e0124377f03d192a49ded
Nicolas Gehlert <nicolas.gehlert@gmail.com>
2022-11-25 17:51:20 +0100

    added base ag grid table

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
-import { NgModule } from '@angular/core';
+import {NgModule, Type} from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'table',
+
+    loadChildren: () => import ('./table/table.module')
+      .then((module: {TableModule: Type<any>}) => module.TableModule),
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
