7d8f36307edefc1b4a82f4410d96a4624f330665
Nicolas Gehlert <nicolas.gehlert@gmail.com>
2022-11-25 17:59:07 +0100

    added table pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,14 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'table',
-
     loadChildren: () => import ('./table/table.module')
       .then((module: {TableModule: Type<any>}) => module.TableModule),
-  }
+  },
+  {
+    path: 'table2',
+    loadChildren: () => import ('./table2/table.module')
+      .then((module: {Table2Module: Type<any>}) => module.Table2Module),
+  },
 ];
 
 @NgModule({
